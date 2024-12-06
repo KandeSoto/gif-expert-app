5f43eaba5cd6cb74de8129d43205852b6c5befc2
KandeSoto <Candelario.soto@sparktechs.com>
2024-12-06 12:49:19 -0800

    render category list

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 
 
 export const GifExpertApp = () => {
+  
+  const [ categories, setCategories ] = useState( ['One Punch', 'Dragon Ball'] );
+
   return (
     <>
+      {/* Titulo */}
       <h1>GifExpertApp</h1>
+      {/* Input */}
+      {/* Listado de Gif */}
+      <ol>
+        { categories.map( category => {
+          return <li key={ category }>{ category }</li>;
+        })}
+      </ol>
+        {/* Gif Item */}
     </>
   )
 }
