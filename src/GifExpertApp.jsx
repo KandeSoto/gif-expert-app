72317bf4c509658c4e68c221fe5f5812ee5a49c9
KandeSoto <Candelario.soto@sparktechs.com>
2024-12-06 13:05:48 -0800

    added add category function

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,12 +5,17 @@ export const GifExpertApp = () => {
   
   const [ categories, setCategories ] = useState( ['One Punch', 'Dragon Ball'] );
 
+  const onAddCategory = () => {    
+    setCategories([ "Ado", ...categories ]);
+  }
+
   return (
     <>
       {/* Titulo */}
       <h1>GifExpertApp</h1>
       {/* Input */}
       {/* Listado de Gif */}
+      <button onClick={ onAddCategory }>Agregar</button>
       <ol>
         { categories.map( category => {
           return <li key={ category }>{ category }</li>;
