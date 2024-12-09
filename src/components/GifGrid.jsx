c832574865c11ca6856dbc86dee304c53c8d0d81
KandeSoto <Candelario.soto@sparktechs.com>
2024-12-09 12:39:48 -0800

    implement load images

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
-import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {  
 
-  useEffect( () => {
-    getGifs(category);
-
-  }, []);
-
+  const { images, isLoading } = useFetchGifs( category );
 
   return (
     <>
       <h3>{ category }</h3>
-      <p>Hola Mundo</p>
+      
+      <div className="card-grid">
+        {
+          images.map( (image) => (
+              <GifItem 
+                key={ image.id } 
+                { ...image }
+              />
+          ))
+        }
+      </div>
     </>
   )
 }
